5c8bd82dd4a25780c53cae158ae24053bdf5c031
pleomax13 <dimonoid9595@gmail.com>
2024-04-18 19:19:53 +0300

    add build

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pwa-test/sw.js', { scope: '/pwa-test/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
